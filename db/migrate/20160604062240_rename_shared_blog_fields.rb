78d4dc6c6aa10bff1e075b3a9c6bc72d7b0a9059
sukanta-mangal <sukanta.mangal@mindfiresolutions.com>
2016-06-07 17:04:55 +0530

    deleted file db/migrate/20160604062240_rename_shared_blog_fields.rb

--- db/migrate/20160604062240_rename_shared_blog_fields.rb
+++
@@ -1,6 +0,0 @@
-class RenameSharedBlogFields < ActiveRecord::Migration
-  def self.up
-    rename_column :shared_blogs, :users_id, :user_id
-    rename_column :shared_blogs, :blogs_id, :blog_id
-  end
-end
