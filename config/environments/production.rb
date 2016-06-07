97f9ebc4b151ec05401ace5c3f6a0ff7935a6def
sukanta-mangal <sukanta.mangal@mindfiresolutions.com>
2016-06-07 22:44:01 +0530

    add configure setting in production.rb to send assets in mailer tamplate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.asset_host                 = "http://secure-retreat-24908.herokuapp.com/"
+  config.action_mailer.default_url_options = "secure-retreat-24908.herokuapp.com/"
+
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
