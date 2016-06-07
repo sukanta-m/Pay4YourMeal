af850247cc630d15edbc326bd1b5d522e8a05e80
sukanta-mangal <sukanta.mangal@mindfiresolutions.com>
2016-06-07 12:50:41 +0530

    configuring email in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'localhost:3000',
+      :user_name            => ENV['EMAIL_ADDRESS'],
+      :password             => ENV['PASSWORD'],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+  }
 end
